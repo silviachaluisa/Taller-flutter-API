fa3e97068bbb6316780e056dd543b7fed222c470
silviachaluisa <veronica-14-1998@hotmail.com>
2024-12-13 16:23:34 -0500

    Add internet permission and enhance error handling in API service

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -6,27 +6,49 @@ class ApiService {
 
   // Función para obtener una lista de nombres de Pokémon
   Future<List<String>> fetchPokemonNames() async {
-    final response = await http.get(Uri.parse(baseUrl + "?limit=100"));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final List<String> names = [];
-      for (var pokemon in data['results']) {
-        names.add(pokemon['name']);
+    try {
+      final url = Uri.parse(baseUrl + "?limit=100");
+      print("Realizando solicitud para obtener nombres de Pokémon en URL: $url");
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final List<String> names = [];
+        for (var pokemon in data['results']) {
+          names.add(pokemon['name']);
+        }
+        print("Lista de nombres obtenida: $names");
+        return names;
+      } else {
+        throw Exception(
+            "Error al obtener la lista de nombres de Pokémon. Código de estado: ${response.statusCode}");
       }
-      return names;
-    } else {
-      throw Exception("Error al obtener la lista de nombres de Pokémon");
+    } catch (e) {
+      print("Error al realizar solicitud para nombres de Pokémon: $e");
+      throw Exception("No se pudo cargar la lista de Pokémon.");
     }
   }
 
   // Función para buscar un Pokémon por nombre
   Future<Map<String, dynamic>> fetchPokemon(String name) async {
-    final url = baseUrl + "$name";
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Error al cargar datos para el Pokémon: $name');
+    try {
+      final url = Uri.parse(baseUrl + "$name");
+      print("Realizando solicitud para obtener detalles del Pokémon en URL: $url");
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print("Datos obtenidos para el Pokémon: $data");
+        return data;
+      } else {
+        throw Exception(
+            'Error al cargar datos para el Pokémon: $name. Código de estado: ${response.statusCode}');
+      }
+    } catch (e) {
+      print("Error al realizar solicitud para el Pokémon: $e");
+      throw Exception("No se pudo cargar los datos del Pokémon.");
     }
   }
 }
